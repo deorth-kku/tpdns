7b7ecfbdfdfb2f1cf3ed0cc711e34bd71d83bc98
deorth-boffin <deorth@outlook.com>
2023-04-25 09:10:10 +0800

    add getwaninfo

---
+++ tpapi/getwaninfo.go
@@ -0,0 +1,29 @@
+package tpapi
+
+type Network struct {
+	WanStatus  WanInfo `json:"wan_status"`
+	WanStatus2 WanInfo `json:"wan_status_2"`
+}
+
+type WanInfo struct {
+	IPAddr     string `json:"ipaddr"`
+	Netmask    string `json:"netmask"`
+	Gateway    string `json:"gateway"`
+	PriDNS     string `json:"pri_dns"`
+	SndDNS     string `json:"snd_dns"`
+	LinkStatus int    `json:"link_status"`
+	ErrorCode  int    `json:"error_code"`
+	Proto      string `json:"proto"`
+	UpTime     int    `json:"up_time"`
+	UpSpeed    int    `json:"up_speed"`
+	DownSpeed  int    `json:"down_speed"`
+	PhyStatus  int    `json:"phy_status"`
+}
+
+const Getwaninfodata = `{"network": {"name": ["wan_status"]},"method": "get"}`
+
+func (s *TPSession) Getwaninfo(timeout int) (info WanInfo, err error) {
+	rsp, err := s.ApiPost(timeout, Getwaninfodata)
+	info = rsp.Network.WanStatus
+	return
+}
