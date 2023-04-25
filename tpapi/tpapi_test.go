d67d505128058d435c7901952f06f703621df85a
deorth-boffin <deorth@outlook.com>
2023-04-25 10:43:08 +0800

    ready for use now

---
+++ tpapi/tpapi_test.go
@@ -0,0 +1,61 @@
+package tpapi
+
+import (
+	"testing"
+
+	"github.com/deorth-kku/tpdns/config"
+)
+
+func getconn(t *testing.T) TPSession {
+	conf, err := config.ReadConf("../config.json")
+	if err != nil {
+		t.Error(err)
+	}
+	c := TPSessionStok(conf.Router.Url, conf.Router.Stok)
+	return c
+}
+
+func TestPasswdEncryption(t *testing.T) {
+	result := passwdEncryption("123456")
+	expected := "0KcgeXhc9TefbwK"
+	if result != expected {
+		t.Errorf("PasswdEncryption('123456') = %s; expected %s", result, expected)
+	}
+}
+
+func TestGethostinfo(t *testing.T) {
+	c := getconn(t)
+	ds, err := c.Gethostsinfo(5)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(ds) == 0 {
+		t.Error("Gethostsinfo returned empty list")
+	}
+}
+
+func TestGetwaninfo(t *testing.T) {
+	c := getconn(t)
+	d, err := c.Getwaninfo(5)
+	if err != nil {
+		t.Error(err)
+	}
+	if d.IPAddr == "" {
+		t.Error("Getwaninfo returned empty info")
+	}
+}
+
+func TestGetboth(t *testing.T) {
+	c := getconn(t)
+	d, err := c.ApiPost(5, Gethostsinfodata, Getwaninfodata)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(d.HostsInfo.HostInfo) == 0 {
+		t.Error("Gethostsinfo returned empty list")
+	}
+	if d.Network.WanStatus.IPAddr == "" {
+		t.Error("Getwaninfo returned empty info")
+	}
+
+}
