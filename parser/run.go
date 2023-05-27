e6a2648f6763be3bc41abc212ac52ac03a0e6070
deorth-boffin <deorth@outlook.com>
2023-05-27 17:16:40 +0800

    reverse dns (only v4 for now)

--- parser/run.go
+++ parser/run.go
@@ -74,3 +74,56 @@ func (gdata *dns_parser) HandleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
 
 	w.WriteMsg(m)
 }
+
+func (dp *dns_parser) HandlePtrRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		dp.parsePtrQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+const PtrSuffix = "in-addr.arpa."
+
+func (dp *dns_parser) parsePtrQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		if !strings.HasSuffix(q.Name, PtrSuffix) {
+			log.Printf("unsupported name for PTR: %s", q.Name)
+			continue
+		}
+		reverse_ip := strings.TrimSuffix(q.Name, PtrSuffix)
+		reverse_ip_slice := strings.Split(reverse_ip, ".")
+		if len(reverse_ip_slice) != 5 {
+			log.Printf("invaild PTR request %s", q.Name)
+		}
+		ip := strings.Join(reverse(reverse_ip_slice[0:4]), ".")
+		switch q.Qtype {
+		case dns.TypePTR:
+			for name, info := range dp.dns_cache {
+				if ip == info.IP {
+					line := fmt.Sprintf("%s %d IN PTR %s.%s", q.Name, dp.countdown, name, dp.Conf.Domain.PrivZone)
+					rr, err := dns.NewRR(line)
+					if err == nil {
+						m.Answer = append(m.Answer, rr)
+					} else {
+						log.Panic(err)
+					}
+					break
+				}
+			}
+		}
+	}
+}
+
+func reverse(a []string) []string {
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+	return a
+}
