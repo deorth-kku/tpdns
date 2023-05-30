9cd9623878fce2d9b4548172bfe69577aa594dba
deorth-boffin <deorth@outlook.com>
2023-05-30 20:20:32 +0800

    rework device fwrules

--- config/fwrules.go
+++ config/fwrules.go
@@ -1,5 +1,7 @@
 package config
 
+type DevicesFwRules []DeviceFwRules
+
 type DeviceFwRules struct {
 	Name  string `json:"name"`
 	Mac   string `json:"mac"`
@@ -9,4 +11,16 @@ type DeviceFwRules struct {
 type Rule struct {
 	Proto string `json:"proto"`
 	Port  uint16 `json:"port"`
+}
+
+func (dfr DevicesFwRules) SearchDevice(name string, mac string) (rules []Rule, exist bool) {
+	for _, conf_dev := range dfr {
+		match_mac := conf_dev.Mac == "" || mac == conf_dev.Mac
+		match_name := conf_dev.Name == "" || name == conf_dev.Name
+		if match_mac && match_name {
+			exist = true
+			rules = conf_dev.Rules
+		}
+	}
+	return
 }
